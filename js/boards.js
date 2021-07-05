d77a260950435eeed593495f0eab168c351a128a
andre9050@yahoo.com <andre9050@yahoo.com>
2021-07-04 22:34:48 -0400

    Added path class

--- js/boards.js
+++ js/boards.js
@@ -7,3 +7,14 @@ class Board {
         this._units = []
     }
 }
+
+// A path of points for units to travel down
+class Path {
+    // Note: a path is basically an array of [x, y] pairs
+    constructor(points = [[0, 0], [10, 10]]) {
+        this._points = []
+    }
+    get point(n) {
+        return this._points[n]
+    }
+}
