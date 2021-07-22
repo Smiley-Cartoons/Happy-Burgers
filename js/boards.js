591d4b86a28960514865f8f9aa6f9a8014122b49
andre9050@yahoo.com <andre9050@yahoo.com>
2021-07-21 23:59:14 -0400

    Added board resizing when window is resized

--- js/boards.js
+++ js/boards.js
@@ -21,15 +21,36 @@ class Board {
     _tick() {
         // TODO: Position units
     }
+
+    // To be added as an event listener to the window.
+    boardOnWindowResize() {
+        let board = globalBoard // this works if a global Board is defigned. // TODO: fix this janky code
+        // Get width and height of the window excluding scrollbars
+        let w = document.documentElement.clientWidth;
+        let h = document.documentElement.clientHeight;
+
+        let boardRatio = board.x_spaces / board.y_spaces
+        let windowRatio = w / h
+        if (boardRatio > windowRatio) {
+            // window is narrower than board
+            board.space_size = h / board.y_spaces
+        } else {
+            // window is stretched more than board
+            board.space_size = w / board.x_spaces
+        }
+        let canvas = document.getElementById("game_board_element")
+        canvas.width = board.space_size * board.x_spaces
+        canvas.height = board.space_size * board.y_spaces
+    }
 }
 
 // A path of points for units to travel down
 class Path {
     // Note: a path is basically an array of [x, y] pairs
     constructor(points = [[0, 0], [10, 10]]) {
-        this._points = []
+        this._points = points
     }
-    get point(n) {
+    point(n) {
         return this._points[n]
     }
 }
